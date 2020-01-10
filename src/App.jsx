a5b63fdd3834851fc82fa698ef7dc4ca4d7f3bd5
Gino Messmer <git@ginomessmer.me>
2020-01-10 17:36:55 +0100

    Add dark mode, optimize search query for downloads

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,38 @@
 
 import React, { useState, useEffect } from 'react';
 import { ListItem } from './components'
-import { Button, ButtonAppearance, Heading, Divider } from "@microsoft/fast-components-react-msft";
+import { Progress } from "@microsoft/fast-components-react-msft";
 
 import './App.css';
+import useDarkMode from 'use-dark-mode';
+import { DesignSystemProvider } from '@microsoft/fast-jss-manager-react';
 
 const App = () => {
+  const darkMode = useDarkMode();
+  const designSystem = {
+    backgroundColor: darkMode.value ? '#333' : '#FFF'
+  };
+
   const [downloads, setDownloads] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    chrome.downloads.search({}, d => {
+    chrome.downloads.search({state: 'complete'}, d => {
       setDownloads(d);
+      setLoading(false);
     });
   }, []);
 
+  console.log(JSON.stringify(downloads));
+
   return (
-    <>
-      <Heading tag={"h1"} size={5}>Downloads</Heading>
-      <Divider />
-      {downloads && downloads.length > 0 && <div>
+    <DesignSystemProvider designSystem={designSystem}>
+      {isLoading && <Progress />}
+
+      {downloads && downloads.length > 0 && !isLoading && <div>
         {downloads.map(d => <ListItem key={d.id} download={d} />)}
       </div>}
-    </>
+    </DesignSystemProvider>
   );
 }
   
