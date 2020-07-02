9e13024de67b9f0efb5cdb60f49c9498703692d7
Gino Messmer <git@ginomessmer.me>
2020-07-02 20:06:34 +0200

    Add observable to download item

--- ng/src/app/downloads/download-item/download-item.component.ts
+++ ng/src/app/downloads/download-item/download-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, NgZone } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-download-item',
@@ -8,6 +9,7 @@ import { Component, OnInit, Input, NgZone } from '@angular/core';
 export class DownloadItemComponent implements OnInit {
 
   @Input() item: chrome.downloads.DownloadItem;
+  item$: Observable<chrome.downloads.DownloadItem>;
   thumbnailUrl: string;
 
   get fileTitle() {
@@ -18,11 +20,21 @@ export class DownloadItemComponent implements OnInit {
     return this.item.state == 'complete';
   }
 
+  get isInProgress() {
+    return this.item.state == 'in_progress';
+  }
+
   constructor(private zone: NgZone) { }
 
   ngOnInit(): void {
     chrome.downloads.getFileIcon(this.item.id, iconUrl => {
       this.zone.run(() => this.thumbnailUrl = iconUrl);
+    });
+
+    this.item$ = new Observable<chrome.downloads.DownloadItem>(observer => {
+      let interval = setInterval(() => chrome.downloads.search({ id: this.item.id }, res => {
+        observer.next(res[0]);
+      }), 500);
     });
   }
 
--- ng/src/app/downloads/downloads-list-page/downloads-list-page.component.ts
+++ ng/src/app/downloads/downloads-list-page/downloads-list-page.component.ts
@@ -2,26 +2,29 @@ import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   templateUrl: './downloads-list-page.component.html',
-  styleUrls: ['./downloads-list-page.component.scss']
+  styleUrls: ['./downloads-list-page.component.scss'],
 })
 export class DownloadsListPageComponent implements OnInit {
-
   public downloadItems: chrome.downloads.DownloadItem[] = [];
   searchTerm: string;
 
-  constructor(private zone: NgZone) {
-  }
+  constructor(private zone: NgZone) {}
 
   ngOnInit(): void {
     this.search('');
   }
 
   search(term: string) {
-    chrome.downloads.search({query: term.split(' ')}, items => {
-      this.zone.run(() => {
-        this.downloadItems = items;
-      });
-    });
-  }
+    let searchQuery = {
+      query: term.split(' '),
+      orderBy: ['-startTime']
+    };
 
+    chrome.downloads.search(searchQuery, (items) => {
+        this.zone.run(() => {
+          this.downloadItems = items;
+        });
+      }
+    );
+  }
 }
