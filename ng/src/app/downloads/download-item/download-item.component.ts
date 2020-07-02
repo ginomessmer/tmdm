c5318efdb0a1a9233d0cdbd6e83cef093c67be4f
Gino Messmer <git@ginomessmer.me>
2020-07-02 19:40:26 +0200

    Add open in folder feature and styling for aborted

--- ng/src/app/downloads/download-item/download-item.component.ts
+++ ng/src/app/downloads/download-item/download-item.component.ts
@@ -14,12 +14,24 @@ export class DownloadItemComponent implements OnInit {
     return this.item.filename.split('\\').pop();
   }
 
+  get isCompleted() {
+    return this.item.state == 'complete';
+  }
+
   constructor(private zone: NgZone) { }
 
   ngOnInit(): void {
     chrome.downloads.getFileIcon(this.item.id, iconUrl => {
       this.zone.run(() => this.thumbnailUrl = iconUrl);
     });
+  }
+
+  openFile() {
+    chrome.downloads.open(this.item.id);
+  }
+
+  openFolder() {
+    chrome.downloads.show(this.item.id);
   }
 
 }
